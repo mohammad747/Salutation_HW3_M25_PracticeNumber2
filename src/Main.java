4053356a877bd746be4472ca2d9cad84503db7ae
mohammad <mohammad.hashemi747@gmail.com>
2019-03-12 15:32:36 +0330

    Adds Main class that includes the main method and an object of the FormerLetterWriter class which calling it's overloaded methods.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FormLetterWriter formLetterWriter = new FormLetterWriter();
+
+        formLetterWriter.displaySalutation("hashemi");
+        formLetterWriter.displaySalutation("Mohammad","Hashemi");
+    }
+}
